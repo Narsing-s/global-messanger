829a054ec149149c0867b92df69fb49a4284595d
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-09-03 01:20:06 +0530

    Use Render backend for account API

--- apps/web/public/account-delete.js
+++ apps/web/public/account-delete.js
@@ -1,5 +1,5 @@
 (() => {
-  const API = window.__GM_CONFIG__?.API_URL || (location.hostname === '127.0.0.1' || location.hostname === 'localhost' ? location.origin : 'https://global-messenger-api.narsingbeesetti006.workers.dev');
+  const API = window.__GM_CONFIG__?.API_URL || (location.hostname === '127.0.0.1' || location.hostname === 'localhost' ? location.origin : 'https://global-messanger-backend.onrender.com');
   let installed = false;
 
   const install = () => {
