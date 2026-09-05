1848d1810628f2402c2edb9ae815d83fbedd6daf
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-09-06 00:18:44 +0530

    Bust web cache for premium UI

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'global-messenger-shell-v6';
+const CACHE_NAME = 'global-messenger-shell-v7';
 const APP_SHELL = ['/', '/manifest.webmanifest'];
 
 self.addEventListener('install', event => {
